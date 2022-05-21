72bf1e11981afa5ee9a0f17fab8a0cc8dd2e238b
Nobletk <noble91knight@gmail.com>
2022-05-21 11:17:59 +0200

    Implementing tinylog dependency and creating tinylog properties file in the resources' folder.

--- src/main/java/boardGame/BoardGameApplication.java
+++ src/main/java/boardGame/BoardGameApplication.java
@@ -1,12 +1,12 @@
 package boardGame;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class BoardGameApplication extends Application {
 
