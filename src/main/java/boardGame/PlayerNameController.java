6c377ffda5ed4cf9ac6d178a6ecca808d47e2573
Nobletk <noble91knight@gmail.com>
2022-05-21 23:07:43 +0200

    Implementing a starting scene to take player names before starting the game.

---
+++ src/main/java/boardGame/PlayerNameController.java
@@ -0,0 +1,22 @@
+package boardGame;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PlayerNameController {
+
+    @FXML
+    private void switchScene(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ui.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
